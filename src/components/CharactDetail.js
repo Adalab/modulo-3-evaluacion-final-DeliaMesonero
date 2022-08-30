c433204346f3ce693c18389fea66ccee1b3077a1
DeliaMesonero <delia_mv88@hotmail.com>
2022-08-30 13:41:12 +0200

    add house Icon

--- src/components/CharactDetail.js
+++ src/components/CharactDetail.js
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 import "../styles/CharactDetail.scss";
+import gryf from "../images/gryf.jpg";
+import huf from "../images/hufflepuff.jpg";
+import raven from "../images/ravenclaw.jpg";
+import sly from "../images/Slytherin.jpg";
 
 function CharactDetail(props) {
-  console.log(props);
   const alive = (value) => {
     if (value === true) {
       return <p>Vivo</p>;
     } else {
       return <p>Muerto</p>;
+    }
+  };
+
+  const houseIcon = () => {
+    if (props.user.house === "Gryffindor") {
+      return <img className="ImgHouse" src={gryf} alt="Gryffindor" />;
+    } else if (props.user.house === "Hufflepuff") {
+      return <img className="ImgHouse" src={huf} alt="Hufflepuff" />;
+    } else if (props.user.house === "Ravenclaw") {
+      return <img className="ImgHouse" src={raven} alt="Ravenclaw" />;
+    } else {
+      return <img className="ImgHouse" src={sly} alt="Slytherin" />;
     }
   };
   const aliveIcon = () => {
@@ -39,13 +54,16 @@ function CharactDetail(props) {
               {" "}
               Nombre: {props.user.name}
             </h4>
-            <p className="detailInfo">Casa: {props.user.house}</p>
+            <p className="detailInfo">
+              Casa: {props.user.house}
+              {houseIcon()}
+            </p>
             <p className="detailInfo">
               Especie:
               {props.user.species}{" "}
             </p>
             <p className="detailInfo">Género:{props.user.gender}</p>
-            <p className="detailInfo">
+            <p className="detailSatatus detailInfo ">
               <span>Estatus:{alive(props.user.alive)}</span>
               <span className="aliveIcon">{aliveIcon(props.user.alive)}</span>
             </p>
